4e9baa3e6ce695cde915a9b53319decab2e39921
cnapolea <cjnapoleao.dev@gmail.com>
2021-08-19 22:21:56 +0100

    complete itereation 1-3

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// jshint esversion:10
+
 const express = require('express');
 
 const hbs = require('hbs');
@@ -13,13 +15,30 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
 // ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', {
+    beerImg: '/images/beer.png'
+  });
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.log(error.message);
+    });
+  res.render('beers');
+});
+
+app.get('/random-beer', (req, res) => {
+  res.render('random_beers');
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
