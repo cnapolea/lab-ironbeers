773dccf1526ecc5d31eb2b318af88e4c55f629b0
cnapolea <cjnapoleao.dev@gmail.com>
2021-08-27 02:00:30 +0100

    style: style beer page create: error page 1. add error handling middleware to app.js 2. redirect error catchs to error template 3. create error template 6. create error route 5. style error template c

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // jshint esversion:10
 
 const express = require('express');
-
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
@@ -13,32 +12,57 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use((err, req, res, next) => {
+  res.redirect('/error');
+});
+
 
 // Register the location for handlebars partials here:
 hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
 // ...
 
+hbs.registerHelper('checklength', (description, length) => {
+  if (description.length > length) {
+    return description.slice(0, length) + ' ...';
+  } else return description;
+});
+
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index', {
-    beerImg: '/images/beer.png'
-  });
-});
-
-app.get('/beers', (req, res) => {
-  punkAPI.getBeers()
-    .then(data => {
-      console.log(data);
-    })
-    .catch(error => {
-      console.log(error.message);
+    res.render('index', {
+      beerImg: '/images/beer.png'
     });
-  res.render('beers');
-});
+  })
+  .get('/beers', (req, res) => {
+    punkAPI.getBeers()
+      .then(data => {
+        console.log(req);
+        res.render('beers', {
+          Beers: data,
+        });
+      })
+      .catch(err => {
+        next(err);
+      });
+  })
+  .get('/random-beer', (req, res) => {
+    const randomBeer = punkAPI.getRandom()
+      .then(beer => {
+        res.render('random_beers', {
+          beer
+        });
+      })
+      .catch(err => next(err));
+  })
+  .get('/error', (req, res) => {
+    res.render('error', {
+      status: 404,
+    });
+  });
 
-app.get('/random-beer', (req, res) => {
-  res.render('random_beers');
+app.get('*', (req, res) => {
+  res.redirect('/error');
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
